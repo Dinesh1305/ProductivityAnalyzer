b045f0421fa7c600a00910ee42d0751ed507e857
Dinesh1305 <165344972+Dinesh1305@users.noreply.github.com>
2025-07-12 15:31:27 +0530

    The ProblemController was updated

--- src/main/java/com/demo/controller/ProblemController.java
+++ src/main/java/com/demo/controller/ProblemController.java
@@ -2,6 +2,8 @@ package com.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -22,9 +24,15 @@ public class ProblemController {
 		return "index";
 	}
 	
-
 	
-	
-	
-	
+	@GetMapping("getProblems")
+	public String get(Model m)
+	{
+		
+		
+		m.addAttribute("problemsList", repo.findAll());
+		
+		return "index";
+		
+	}
 }
