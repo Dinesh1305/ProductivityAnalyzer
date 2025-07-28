e9df52d98d53c8db2b7a19faf8cd6662a71ab245
Dinesh1305 <165344972+Dinesh1305@users.noreply.github.com>
2025-07-28 21:20:43 +0530

    The work.java entity was created

---
+++ src/main/java/com/demo/model/CompleteWork.java
@@ -0,0 +1,42 @@
+package com.demo.model;
+
+import java.net.URL;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+@Entity
+public class CompleteWork {
+@Id
+private Integer id;
+
+private String work;
+
+private URL url;
+
+public Integer getId() {
+	return id;
+}
+
+public void setId(Integer id) {
+	this.id = id;
+}
+
+public String getWork() {
+	return work;
+}
+
+public void setWork(String work) {
+	this.work = work;
+}
+
+public URL getUrl() {
+	return url;
+}
+
+public void setUrl(URL url) {
+	this.url = url;
+}
+
+
+
+}
---
+++ src/main/java/com/demo/model/Works.java
@@ -0,0 +1,40 @@
+package com.demo.model;
+
+import java.net.URL;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Works {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+	private String work;
+	private URL url;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getWork() {
+		return work;
+	}
+	public void setWork(String work) {
+		this.work = work;
+	}
+	public URL getUrl() {
+		return url;
+	}
+	public void setUrl(URL url) {
+		this.url = url;
+	}
+	
+
+	
+	
+}
