4c25790284bfc203cb8746ab6b2a69135cb1dd58
Dinesh1305 <165344972+Dinesh1305@users.noreply.github.com>
2025-08-21 22:45:39 +0530

    The Users class was created for managing user detail

---
+++ src/main/java/com/demo/model/Users.java
@@ -0,0 +1,57 @@
+package com.demo.model;
+
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Users {
+@Id
+@GeneratedValue(strategy=GenerationType.IDENTITY)
+private UUID id;
+
+private String name;
+
+
+private String email;
+
+
+
+
+
+
+public UUID getId() {
+	return id;
+}
+
+
+public void setId(UUID id) {
+	this.id = id;
+}
+
+
+public String getName() {
+	return name;
+}
+
+
+public void setName(String name) {
+	this.name = name;
+}
+
+
+public String getEmail() {
+	return email;
+}
+
+
+public void setEmail(String email) {
+	this.email = email;
+}
+
+
+
+}
