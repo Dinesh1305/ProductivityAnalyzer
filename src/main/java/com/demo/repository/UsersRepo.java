dd5863ed352ecd9ccfe74b0964befcecaa7da712
Dinesh1305 <165344972+Dinesh1305@users.noreply.github.com>
2025-08-21 22:52:18 +0530

    The UsersRepo was created

---
+++ src/main/java/com/demo/repository/UsersRepo.java
@@ -0,0 +1,11 @@
+package com.demo.repository;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.demo.model.Users;
+
+public interface UsersRepo extends JpaRepository<Users,UUID>{
+
+}
