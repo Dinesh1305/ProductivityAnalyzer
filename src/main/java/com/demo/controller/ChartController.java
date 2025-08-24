3b77aae98e277621883dbb9fa0425b7c2621fdb2
Dinesh1305 <165344972+Dinesh1305@users.noreply.github.com>
2025-08-24 10:18:16 +0530

    TreeMap was added in chartcontroller class to dipslay the data in ascending order

--- src/main/java/com/demo/controller/ChartController.java
+++ src/main/java/com/demo/controller/ChartController.java
@@ -1,18 +1,19 @@
 package com.demo.controller;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.demo.model.CompleteTasks;
 import com.demo.model.Tasks;
-import com.demo.repository.CompleteTaskRepo;
+import com.demo.service.ChartService;
 import com.demo.service.CompleteTaskService;
 
 @RestController
@@ -22,106 +23,107 @@ public class ChartController {
 
 	@Autowired
 	CompleteTaskService service;
-	
+
 	@Autowired
-	CompleteTaskRepo repo;
+	ChartService service2;
 
+	@GetMapping("/today")
+	public Map<String, Object> getToday() {
+		Map<String, Object> response = new HashMap<>();
 
-    
-    @GetMapping("/today")
-    public Map<String, Object> getToday() {
-        Map<String, Object> response = new HashMap<>();
+		List<Tasks> t = service.getToday();
 
-        List<Tasks>t=service.getToday();
-        
+		List<String> work = new ArrayList<>();
 
-        
-        List<String>work=new ArrayList<>();
-        
-        List<Long>duration=new ArrayList<>();
-        
-        for(Tasks temp:t)
-        {
-        	work.add(temp.getWork());
-        	
-        	duration.add(temp.getTotalSeconds());
-        }
-        // Labels for x-axis
-        response.put("labels", work);
+		List<Long> duration = new ArrayList<>();
 
-        // Data for y-axis
-        response.put("data", duration);
+		for (Tasks temp : t) {
+			work.add(temp.getWork());
 
-        return response;
+			duration.add(temp.getTotalSeconds());
+		}
+		// Labels for x-axis
+		response.put("labels", work);
 
-}
-    
-    @GetMapping("/week")
-    public Map<String, Object> getWeek() {
-        Map<String, Object> response = new HashMap<>();
+		// Data for y-axis
+		response.put("data", duration);
 
-        List<Tasks>t=service.getWeek();
-        
-        System.out.println(t);
-        System.out.println(t);
-        List<Tasks>t2=repo.getFullList();
-        
-        List<String>work=new ArrayList<>();
-        
-        List<Long>duration=new ArrayList<>();
-        
-        for(Tasks temp:t)
-        {
-        	work.add(temp.getWork());
-        	
-        	duration.add(temp.getTotalSeconds());
-        }
-        // Labels for x-axis
-        response.put("labels", work);
+		return response;
 
-        // Data for y-axis
-        response.put("data", duration);
+	}
 
-        return response;
+	@GetMapping("/week")
+	public Map<String, Object> getWeek() {
+		Map<String, Object> response = new HashMap<>();
 
-}
-    
-    @GetMapping("/full")
-    public Map<String, Object> getFull() {
-        Map<String, Object> response = new HashMap<>();
+		List<Tasks> t = service.getWeek();
 
-        List<Tasks>t=service.getAll();
-        
-     //  System.out.println(t);
-        List<Tasks>t1=repo.getFullList();
-       
-        
-        for(Tasks y:t1)
-        {
-        	 System.out.println(y);
-        }
-        
-        List<String>work=new ArrayList<>();
-        
-        List<Long>duration=new ArrayList<>();
-        
-        for(Tasks temp:t1)
-        {
-        	work.add(temp.getWork());
-        	
-        	duration.add(temp.getTotalSeconds());
-        }
-        // Labels for x-axis
-        response.put("labels", work);
+		List<String> work = new ArrayList<>();
 
-        // Data for y-axis
-        response.put("data", duration);
+		List<Long> duration = new ArrayList<>();
+	
 
-        return response;
+		
+		Map<Long,String>map=new TreeMap<>(Collections.reverseOrder());
+		
+		
+for (Tasks temp : t) {
+			
+			
+		
 
-}
-    
-    
-    
-  
+		map.put(temp.getTotalSeconds(),temp.getWork());
+		}
+		
+		for (Long temp : map.keySet()) {
+		
+			duration.add(temp);
+			work.add(map.get(temp));
+		}
+		// Labels for x-axis
+		response.put("labels", work);
+
+		// Data for y-axis
+		response.put("data", duration);
+
+		return response;
+
+	}
+
+	@GetMapping("/full")
+	public Map<String, Object> getFull() {
+		Map<String, Object> response = new HashMap<>();
+
+		List<Tasks> t1 = service2.FullList();
+
+		List<String> work = new ArrayList<>();
+
+		List<Long> duration = new ArrayList<>();
+
+		Map<Long,String>map=new TreeMap<>(Collections.reverseOrder());
+		
+		
+for (Tasks temp : t1) {
+			
+			
+		
+
+		map.put(temp.getTotalSeconds(),temp.getWork());
+		}
+		
+		for (Long temp : map.keySet()) {
+		
+			duration.add(temp);
+			work.add(map.get(temp));
+		}
+		// Labels for x-axis
+		response.put("labels", work);
+
+		// Data for y-axis
+		response.put("data", duration);
+
+		return response;
+
+	}
+
 }
