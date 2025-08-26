68e1352b024f3c771f1f735c59574cd862442680
Dinesh1305 <165344972+Dinesh1305@users.noreply.github.com>
2025-08-26 21:51:15 +0530

    ProblemService was created for manages the ProblemRepo related task

---
+++ src/main/java/com/demo/service/ProblemService.java
@@ -0,0 +1,30 @@
+package com.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.demo.model.Problems;
+import com.demo.repository.ProblemRepo;
+
+@Service
+public class ProblemService {
+
+	@Autowired
+	ProblemRepo repo;
+
+	public void add(Problems problem) {
+		repo.save(problem);
+
+	}
+
+	public List<Problems> getAll() {
+		return repo.findAll();
+	}
+
+	public void delete(String name) {
+		repo.deleteById(name);
+	}
+
+}
