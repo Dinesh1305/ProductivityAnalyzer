7739610d8fcd7ddf180e0dfa46b951b97a08f1ee
Dinesh1305 <165344972+Dinesh1305@users.noreply.github.com>
2025-08-28 14:23:10 +0530

    Code_MoniterController was updated

--- src/main/java/com/demo/controller/Code_MoniterController.java
+++ src/main/java/com/demo/controller/Code_MoniterController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -14,20 +15,21 @@ import reactor.core.publisher.Mono;
 @RestController
 public class Code_MoniterController {
 
-	@GetMapping("getDetails")
-	public String details() {
+	@GetMapping("getDetails/{name}")
+	public String details(@PathVariable String name) {
 		WebClient client = WebClient.create("https://leetcode.com");
 
-		String query = """
+	
+		String query = String.format("""
 				{
-				  recentSubmissionList(username: "akesh_007") {
+				  recentSubmissionList(username: "%s") {
 				    title
 
 				    status
 
 				  }
 				}
-				""";
+				""",name);
 
 		Mono<ResponseEntity<String>> result = client.post().uri("/graphql")
 				.header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE).bodyValue(Map.of("query", query))
