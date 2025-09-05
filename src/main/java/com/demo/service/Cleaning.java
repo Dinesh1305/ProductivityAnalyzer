dc30407077137025506b75021cf4e42feb0f2c28
Dinesh1305 <165344972+Dinesh1305@users.noreply.github.com>
2025-09-05 16:49:34 +0530

    The bug was fixed (bug even application stop the task still runs)

--- src/main/java/com/demo/service/Cleaning.java
+++ src/main/java/com/demo/service/Cleaning.java
@@ -1,16 +1,30 @@
 package com.demo.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import com.demo.model.Task;
 
 import jakarta.annotation.PreDestroy;
 @Component
 public class Cleaning {
 
+@Autowired
+TaskService service;
 
+	
 	@PreDestroy
 	public void close()
 	{
+		Long count= service.getCount();
+		List<Task>t=service.get();
+		if(count==1)
+		{
+		service.remove(t.get(0).getId());	
 		
+		}
 	}
 	
 }
